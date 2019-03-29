97a036aac2d9ca9bf6a7bd89168e855bf12dfea0
Darko Zivkovic <darkozivkovic@Darkos-Mac-mini.local>
2019-03-29 14:35:56 +0100

    added widget size to export

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
-import { Widget } from './src/widget';
+import { Widget, WidgetSize } from './src/widget';
 import {PackeryModule } from './src/packery.module';
 
 export {
     Widget,
+    WidgetSize,
     PackeryModule
 }
